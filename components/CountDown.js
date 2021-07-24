aff33ef9bfa993e092f436afe3d01f7bb5c5f6b2
John Glennan <glennan20@gmail.com>
2021-07-23 23:01:54 -0400

    added countdown rfc

---
+++ components/CountDown.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, StyleSheet, Text } from "react-native";
+
+export default function CountDown() {
+    return (
+        <View>
+            <Text>Test<Text />
+        </View>
+        
+    )
+}
