6067b7ee8386abd69863323b07e34c17794c8586
glennanj1 <glennan20@gmail.com>
2021-11-25 15:17:09 -0500

    Fixed styling added different color scheme

--- containers/HomeScreen.js
+++ containers/HomeScreen.js
@@ -6,9 +6,9 @@ import { StatusBar } from 'expo-status-bar';
 export default function HomeScreen ({ navigation }) {
     return (
         <View style={styles.container} >
-              <Text style={styles.header}>Welcome to Pomodoro Timer</Text>
+              <Text style={styles.header}>Pomodoro Timer</Text>
             
-            <Text style={styles.header2}>Click below to get started!</Text>
+            <Text style={styles.header2}>A lightweight React Native Demo</Text>
            
             <Pressable 
               style={styles.button}
@@ -16,7 +16,7 @@ export default function HomeScreen ({ navigation }) {
                 navigation.navigate('Timer')
               }}
               >
-                <Text style={styles.text}>Timer</Text>
+                <Text style={styles.text}>Enter</Text>
               </Pressable>
         
             <StatusBar style="auto" />
@@ -29,32 +29,43 @@ export default function HomeScreen ({ navigation }) {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: 'white',
+      backgroundColor: 'grey',
       alignItems: 'center',
       justifyContent: 'center',
+      width: '100%',
+      height: '100%',
+      
     },
     button: {
+      position: 'absolute',
+      bottom: '10%',
       alignItems: 'center',
       justifyContent: 'center',
-      paddingVertical: 12,
-      paddingHorizontal: 32,
-      borderRadius: 4,
-      elevation: 3,
-      backgroundColor: 'black',
+      elevation: 100,
+      fontSize: 40,
+      borderRadius: 10,
+      backgroundColor: 'darkgrey',
+      margin: 10, 
+      padding: 10,
     },
     text: {
-      color: 'white'
+      color: 'white',
+      fontSize: 40,
+      fontWeight: 'bold',
     },
     header: {
-        backgroundColor: 'red',
-        marginBottom: '30px',
-        fontWeight: 'bold'    
+        fontWeight: 'bold',
+        width: '80%',
+        fontSize: 50,
+        padding: 10, 
+        borderColor: 'black',
+        borderWidth: 2,
+        borderRadius: 10,
     },
     header2: {
-        marginBottom: '10px',
+        marginBottom: 10,
         fontWeight: '500',
-    
-    },
-  
-    
+        fontSize: 20,
+        padding: 20, 
+    }, 
   });
